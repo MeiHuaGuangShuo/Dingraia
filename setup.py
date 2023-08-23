44768e3b067f1321773d003cec024a7f31870063
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2023-08-23 12:21:54 +0800

    添加setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+﻿from setuptools import setup, find_packages
+setup(
+    name='dingraia',
+    version='2.0.0',
+    packages=find_packages(),
+    url='https://github.com/MeiHuaGuangShuo/Dingraia',
+    author='MeiHuaGuangShuo',
+    author_email='meihuaguangshuo@gmail.com',
+    description='A Dingtalk robot framework based on Python',
+    install_requires=[
+        "loguru",
+        "requests",
+        "aiohttp",
+        "pycryptodome",
+        "websockets"
+    ],
+    python_requires='>3.7',
+    zip_safe=True,
+)
