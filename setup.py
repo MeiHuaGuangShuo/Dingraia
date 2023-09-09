e352feec4fd851ff46ab8502e489154fe7af73c2
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2023-09-09 23:58:46 +0800

    修改了版本号为2.0.1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 ﻿from setuptools import setup, find_packages
 setup(
     name='dingraia',
-    version='2.0.0',
+    version='2.0.1',
     packages=find_packages(),
     url='https://github.com/MeiHuaGuangShuo/Dingraia',
     author='MeiHuaGuangShuo',
     author_email='meihuaguangshuo@gmail.com',
-    description='A Dingtalk robot framework based on Python',
+    description='A Dingtalk robot framework based on Python, supporting http and stream mode.',
     install_requires=[
         "loguru",
         "requests",
