855abac33c4be97e32f4d9b2ba1650fb1f82a031
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2024-02-17 10:47:35 +0800

    更新 setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 ﻿from setuptools import setup, find_packages
+from dingraia.VERSION import VERSION
 
 setup(
     name='dingraia',
-    version='2.0.3',
+    version=VERSION,
     packages=find_packages(),
     url='https://github.com/MeiHuaGuangShuo/Dingraia',
     author='MeiHuaGuangShuo',
     author_email='meihuaguangshuo@gmail.com',
-    description='A Dingtalk robot framework based on Python, supporting http and stream mode.',
-    long_description='A Dingtalk robot framework based on official, easier and advanced to use.',
+    description='A Dingtalk robot framework based on Python, supported http and stream mode.',
+    long_description='A Dingtalk robot framework, easier to use.',
     install_requires=[
         "loguru",
         "requests",
         "aiohttp",
         "pycryptodome",
-        "websockets"
+        "websockets",
+        "rich"
     ],
     python_requires='>3.7',
     zip_safe=True,
