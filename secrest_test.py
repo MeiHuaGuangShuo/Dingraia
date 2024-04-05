ed61e00ce20d25ed70f127c3fc56ac54b03013fe
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2024-04-05 15:50:56 +0800

    Key detect test

---
+++ secrest_test.py
@@ -0,0 +1,12 @@
+"""
+All secrets are cheat
+
+"""
+OpenAI_API_Key = "dahbfikuw-39fe7hw-dnfSdfalqW2h75Lw"
+MicrosoftKey = "boomnf6ywu2-Hfueh6-hfjd4a8fe-fnkfag73"
+BitcoinKey = "hdf39yfhasj3tgw239874fbysfg6353s"
+PypiToken = "aigf378fa91y-23rguyg76-swfg763uef"
+AmazonKey = "23r9087fs-23r91bhcsa6-13y4b4-w7"
+AmazonSecretKey = "3y897r-fsg873bs0-378gsfewghs5"
+DingtalkAppKey = "dingjhcr789w78hrf8"
+DingtalkAppSecret = "joaUhUgyUftyivkweuyiwOWYgIYOGHjvfksuisygiogsdfbvYboI56G574-ogi7FIYt3htcfTYJf"
