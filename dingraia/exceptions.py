98d4a5d19cf5c1af6bd8e850359e66545b0647c5
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2024-08-13 14:40:33 +0800

    对部分错误提供修正建议

--- dingraia/exceptions.py
+++ dingraia/exceptions.py
@@ -49,18 +49,22 @@ class ResourceNotFoundError(DingtalkAPIError):
 
 
 class WrongParameterError(DingtalkAPIError):
+    solution = "检查参数是否符合规格。具体请参考当前接口的文档的参数说明和参数示例。"
     code = 400002
 
 
 class InvalidParameterError(DingtalkAPIError):
+    solution = "检查下有没有传请求参数，一般发生在http post形式的接口里，没有传参数。"
     code = 40035
 
 
 class InvalidFileTypeError(DingtalkAPIError):
+    solution = "如果是文件类型，检查下是否是支持。目前只支持doc、docx、xls、xlsx、ppt、pptx、zip、pdf、rar。"
     code = 40005
 
 
 class InvalidUserIdError(DingtalkAPIError):
+    solution = "请检查userid是否正确，可通过获取部门用户userid列表接口(Dingtalk.get_dept_users)获取。"
     code = 33012
 
 
@@ -70,18 +74,22 @@ class DepartmentNotExistError(DingtalkAPIError):
 
 
 class ApiPermissionDeniedError(DingtalkAPIError):
+    solution = "需要修改appkey对应的权限点。请上开放平台 > 应用详情页 > 权限管理 > 添加接口权限 > 接口权限勾选对应的权限点。"
     code = 60011
 
 
 class IPNotInWhitelistError(DingtalkAPIError):
+    solution = "企业应用：检查配置的服务器出口ip地址是否和请求ip地址一致; isv应用：检查套件ip白名单和请求ip是否一致。"
     code = 60020
 
 
 class UserNotFoundError(DingtalkAPIError):
+    solution = "检查该企业下该员工是否存在。"
     code = 60121
 
 
 class APIRateLimitedError(DingtalkAPIError):
+    solution = "当前接口调用超过最高频率限制，触发全局限流，请稍后重试。"
     code = 90002
     code2 = 90018
 
