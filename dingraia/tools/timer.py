8716da6cb6a6f0d0cb9d70d8d3c98fc5c25d7762
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2024-09-30 20:04:46 +0800

    新增计时类，方便查看函数耗费时间

--- dingraia/tools/timer.py
+++ dingraia/tools/timer.py
@@ -1,38 +1,59 @@
 ﻿import time
+import inspect
 from ..log import logger
 
 
-class TimeCost:
-    
-    def __init__(self, title=None):
-        self.title = title
+def format_time(seconds):
+    if seconds < 1e-6:
+        return f"{seconds * 1e9:.2f} ns"  # 纳秒
+    elif seconds < 1e-3:
+        return f"{seconds * 1e6:.2f} µs"  # 微秒
+    elif seconds < 1:
+        return f"{seconds * 1e3:.2f} ms"  # 毫秒
+    elif seconds < 60:
+        return f"{seconds:.2f} s"  # 秒
+    elif seconds < 3600:
+        minutes, sec = divmod(seconds, 60)
+        return f"{seconds:.2f} s ({int(minutes)}m {sec:.2f}s)"
+    elif seconds < 86400:
+        hours, minutes = divmod(seconds, 3600)
+        minutes, sec = divmod(minutes, 60)
+        return f"{seconds:.2f} s ({int(hours)}h {int(minutes)}m {sec:.2f}s)"
+    else:
+        days, hours = divmod(seconds, 86400)
+        hours, minutes = divmod(seconds, 3600)
+        minutes, sec = divmod(minutes, 60)
+        return f"{seconds:.2f} s ({int(days)}d {int(hours)}h {int(minutes)}m {sec:.2f}s)"
 
-    @staticmethod
-    def format_time(seconds):
-        if seconds < 1e-6:
-            return f"{seconds * 1e9:.2f} ns"  # 纳秒
-        elif seconds < 1e-3:
-            return f"{seconds * 1e6:.2f} µs"  # 微秒
-        elif seconds < 1:
-            return f"{seconds * 1e3:.2f} ms"  # 毫秒
-        elif seconds < 60:
-            return f"{seconds:.2f} s"  # 秒
-        elif seconds < 3600:
-            minutes, sec = divmod(seconds, 60)
-            return f"{seconds:.2f} s ({int(minutes)}m {sec:.2f}s)"
-        elif seconds < 86400:
-            hours, minutes = divmod(seconds, 3600)
-            minutes, sec = divmod(minutes, 60)
-            return f"{seconds:.2f} s ({int(hours)}h {int(minutes)}m {sec:.2f}s)"
+
+class TimeCost:
+
+    def __init__(self, title=None, print_time: bool = True):
+        self.title = title
+        self.print_time = print_time
+        self.start_time = 0.0
+
+    def func_time(self, func):
+        def wrapper(*args, **kwargs):
+            with self:
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    def __call__(self, *args, **kwargs):
+        if not self.start_time:
+            self.start_time = time.perf_counter()
         else:
-            days, hours = divmod(seconds, 86400)
-            hours, minutes = divmod(seconds, 3600)
-            minutes, sec = divmod(minutes, 60)
-            return f"{seconds:.2f} s ({int(days)}d {int(hours)}h {int(minutes)}m {sec:.2f}s)"
-    
+            self.cost_time = time.perf_counter() - self.start_time
+            if self.print_time:
+                logger.info(f"Time{f'({self.title})' if self.title is not None else ''}: {format_time(self.cost_time)}",
+                            _inspect=inspect.currentframe())
+
     def __enter__(self):
         self.start_time = time.perf_counter()
-    
+
     def __exit__(self, exc_type, exc_val, exc_tb):
-        cost_time = time.perf_counter() - self.start_time
-        logger.info(f"Time{f'({self.title})' if self.title is not None else ''}: {self.format_time(cost_time)}")
+        self.cost_time = time.perf_counter() - self.start_time
+        if self.print_time:
+            logger.info(f"Time{f'({self.title})' if self.title is not None else ''}: {format_time(self.cost_time)}",
+                        _inspect=inspect.currentframe())
