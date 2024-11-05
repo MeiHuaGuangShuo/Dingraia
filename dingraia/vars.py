4f320bcf471134c1837c0546c1e629399de8ebfc
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2024-11-05 14:46:57 +0800

    新增变量

--- dingraia/vars.py
+++ dingraia/vars.py
@@ -6,4 +6,8 @@
 DINGTALK_OAPI = "https://oapi.dingtalk.com"
 DINGTALK_API = "https://api.dingtalk.com"
 MiB = 1024 * 1024
-GiB = 1024* MiB
+GiB = 1024 * MiB
+Second = 1
+Minute = 60 * Second
+Hour = 60 * Minute
+Day = 24 * Hour
