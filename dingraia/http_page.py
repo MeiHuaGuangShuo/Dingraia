922158e2c4bc66ef34d3562492db57cd9623f85c
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2024-12-22 16:10:25 +0800

    更新http界面

--- dingraia/http_page.py
+++ dingraia/http_page.py
@@ -1,23 +1,293 @@
 HTTP_DEFAULT_PAGE = """
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
-    <meta http-equiv="refresh" content="0;url=https://example.com/">
+    <title>Dingraia HTTP Client</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 2em;
+        background-color: #fdfdff;
+        border-radius: 0.5em;
+        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+    @media (max-width: 700px) {
+        div {
+            margin: 0 auto;
+            width: auto;
+        }
+    }
+    </style>    
 </head>
+
 <body>
+<div>
+    <h1>Dingraia HTTP Client</h1>
+    <p>一个支持异步和Stream的钉钉机器人框架，适应于群聊机器人，提供部分简单的API来实现群组操作</p>
+    <p>A frame that use async function to easily build a dingtalk chatbot/robot, support HTTP & Stream connection.</p>
+    <p><a href="https://github.com/MeiHuaGuangShuo/Dingraia">More information...</a></p>
+</div>
 </body>
 </html>
+"""
 
+HTTP_401_PAGE = """
+<!doctype html>
+<html>
+<head>
+    <title>404 - Dingraia HTTP Client</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 2em;
+        background-color: #fdfdff;
+        border-radius: 0.5em;
+        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+    @media (max-width: 700px) {
+        div {
+            margin: 0 auto;
+            width: auto;
+        }
+    }
+    </style>    
+</head>
+
+<body>
+<div>
+    <h1>401 - Dingraia HTTP Client</h1>
+    <p>未授权的访问</p>
+    <p>Unauthorized access.</p>
+    <p><a href="https://github.com/MeiHuaGuangShuo/Dingraia">More information...</a></p>
+</div>
+</body>
+</html>
 """
 
 HTTP_404_PAGE = """
-<>
+<!doctype html>
+<html>
+<head>
+    <title>404 - Dingraia HTTP Client</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 2em;
+        background-color: #fdfdff;
+        border-radius: 0.5em;
+        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+    @media (max-width: 700px) {
+        div {
+            margin: 0 auto;
+            width: auto;
+        }
+    }
+    </style>    
+</head>
+
+<body>
+<div>
+    <h1>404 - Dingraia HTTP Client</h1>
+    <p>您所请求的网页不存在</p>
+    <p>The page you are looking for does not exist.</p>
+    <p><a href="https://github.com/MeiHuaGuangShuo/Dingraia">More information...</a></p>
+</div>
+</body>
+</html>
 """
 
 HTTP_405_PAGE = """
+<!doctype html>
+<html>
+<head>
+    <title>405 - Dingraia HTTP Client</title>
 
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 2em;
+        background-color: #fdfdff;
+        border-radius: 0.5em;
+        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+    @media (max-width: 700px) {
+        div {
+            margin: 0 auto;
+            width: auto;
+        }
+    }
+    </style>    
+</head>
+
+<body>
+<div>
+    <h1>405 - Dingraia HTTP Client</h1>
+    <p>你所请求的方法不被允许</p>
+    <p>The method you are trying to access is not allowed.</p>
+    <p><a href="https://github.com/MeiHuaGuangShuo/Dingraia">More information...</a></p>
+</div>
+</body>
+</html>
 """
 
 HTTP_500_PAGE = """
+<!doctype html>
+<html>
+<head>
+    <title>500 - Dingraia HTTP Client</title>
 
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 2em;
+        background-color: #fdfdff;
+        border-radius: 0.5em;
+        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+    @media (max-width: 700px) {
+        div {
+            margin: 0 auto;
+            width: auto;
+        }
+    }
+    </style>    
+</head>
+
+<body>
+<div>
+    <h1>500 - Dingraia HTTP Client</h1>
+    <p>服务器内部错误</p>
+    <p>An internal server error occurred.</p>
+    <p><a href="https://github.com/MeiHuaGuangShuo/Dingraia">More information...</a></p>
+</div>
+</body>
+</html>
+"""
+
+HTTP_INVALID_RESPONSE_PAGE = """
+<!doctype html>
+<html>
+<head>
+    <title>Error - Dingraia HTTP Client</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 2em;
+        background-color: #fdfdff;
+        border-radius: 0.5em;
+        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+    @media (max-width: 700px) {
+        div {
+            margin: 0 auto;
+            width: auto;
+        }
+    }
+    </style>    
+</head>
+
+<body>
+<div>
+    <h1>Error - Dingraia HTTP Client</h1>
+    <p>响应的结果不是有效的HTTP响应</p>
+    <p>The response is not a valid HTTP response.</p>
+    <p><a href="https://github.com/MeiHuaGuangShuo/Dingraia">More information...</a></p>
+</div>
+</body>
+</html>
 """
