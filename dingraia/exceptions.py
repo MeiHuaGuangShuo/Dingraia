9581e92074720dbd2ded9e563af9efe6f5d3e79c
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2024-12-23 22:25:49 +0800

    更新语言支持

--- dingraia/exceptions.py
+++ dingraia/exceptions.py
@@ -1,6 +1,7 @@
 """
 错误合集，用于提示
 """
+from .i18n import i18n
 
 
 class DingtalkAPIError(Exception):
@@ -32,24 +33,20 @@ class DingtalkAPIError(Exception):
                     if data:
                         msg += f" Response: {data}"
                 else:
-                    msg = f"An Error happened while requesting Dingtalk API. Response: {data}"
+                    msg = i18n.ErrDefaultMsg.format(data=data)
                 if not self.solution:
                     if request_id:
-                        self.solution = (f"使用请求ID '{request_id}' 前往 "
-                                         f"https://open-dev.dingtalk.com/fe/api-tools"
-                                         f"?hash=%23%2Ftroubleshoot#/troubleshoot 查看解决方案")
+                        self.solution = i18n.ErrSolutionRequestId.format(request_id=request_id)
                     elif code:
-                        self.solution = (f"使用错误码 '{code}' 前往 "
-                                         f"https://open-dev.dingtalk.com/fe/api-tools"
-                                         f"?hash=%23%2Ftroubleshoot#/troubleshoot 查看解决方案")
+                        self.solution = i18n.ErrSolutionCode.format(code=code)
             except:
                 pass
         if self.solution:
             msg = str(msg)
             if msg.strip().endswith('.') or msg.strip().endswith('。'):
-                msg += f" Solution: {self.solution}"
+                msg += f" {i18n.ErrSolutionText}: {self.solution}"
             else:
-                msg += f". Solution: {self.solution}"
+                msg += f". {i18n.ErrSolutionText}: {self.solution}"
         super().__init__(msg)
 
 
@@ -86,47 +83,47 @@ class ResourceNotFoundError(DingtalkAPIError):
 
 
 class WrongParameterError(DingtalkAPIError):
-    solution = "检查参数是否符合规格。具体请参考当前接口的文档的参数说明和参数示例。"
+    solution = i18n.WrongParameterErrorSolution
     code = 400002
 
 
 class InvalidParameterError(DingtalkAPIError):
-    solution = "检查下有没有传请求参数，一般发生在http post形式的接口里，没有传参数。"
+    solution = i18n.InvalidParameterErrorSolution
     code = 40035
 
 
 class InvalidFileTypeError(DingtalkAPIError):
-    solution = "如果是文件类型，检查下是否是支持。目前只支持doc、docx、xls、xlsx、ppt、pptx、zip、pdf、rar。"
+    solution = i18n.InvalidFileTypeErrorSolution
     code = 40005
 
 
 class InvalidUserIdError(DingtalkAPIError):
-    solution = "请检查userid是否正确，可通过获取部门用户userid列表接口(Dingtalk.get_dept_users)获取。"
+    solution = i18n.InvalidUserIdErrorSolution
     code = 33012
 
 
 class DepartmentNotExistError(DingtalkAPIError):
-    solution = "可通过获取部门列表接口 (Dingtalk.get_depts) 获取"
+    solution = i18n.DepartmentNotExistErrorSolution
     code = 60003
 
 
 class ApiPermissionDeniedError(DingtalkAPIError):
-    solution = "需要修改appkey对应的权限点。请上开放平台 > 应用详情页 > 权限管理 > 添加接口权限 > 接口权限勾选对应的权限点。"
+    solution = i18n.ApiPermissionDeniedErrorSolution
     code = 60011
 
 
 class IPNotInWhitelistError(DingtalkAPIError):
-    solution = "企业应用：检查配置的服务器出口ip地址是否和请求ip地址一致; isv应用：检查套件ip白名单和请求ip是否一致。"
+    solution = i18n.IPNotInWhitelistErrorSolution
     code = 60020
 
 
 class UserNotFoundError(DingtalkAPIError):
-    solution = "检查该企业下该员工是否存在。"
+    solution = i18n.UserNotFoundErrorSolution
     code = 60121
 
 
 class APIRateLimitedError(DingtalkAPIError):
-    solution = "当前接口调用超过最高频率限制，触发全局限流，请稍后重试。"
+    solution = i18n.APIRateLimitedErrorSolution
     code = 90002
     code2 = 90018
 
