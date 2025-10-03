84b17986d08362202af64bcc1ac726529a6b527a
MeiHuaGuangShuo <meihuaguangshuo@gmail.com>
2025-10-03 18:37:45 +0800

    lazy.py更新，让你更懒！

--- dingraia/lazy.py
+++ dingraia/lazy.py
@@ -1,17 +1,30 @@
-import functools
-import inspect
-from .event.message import *
-from .event.event import *
-from .message.chain import MessageChain
-from .message.element import *
-from .model import *
-from .saya import *
-from .saya.builtins.broadcast import ListenerSchema
-from .DingTalk import Dingtalk
-from .util.async_exec import io_bound, cpu_bound
-from .card import *
-from .log import logger
-from .element import *
-import asyncio
+"""
+Lazy库是给懒人的你用的，但是有得必有失，请注意函数名冲突可能存在
+"""
+import asyncio  # NOQA
+import functools  # NOQA
+import inspect  # NOQA
+
+from .DingTalk import Dingtalk  # NOQA
+from .card import *  # NOQA
+from .element import *  # NOQA
+from .event.event import *  # NOQA
+from .event.message import *  # NOQA
+from .log import logger  # NOQA
+from .message.chain import MessageChain  # NOQA
+from .message.element import *  # NOQA
+from .model import *  # NOQA
+from .saya import *  # NOQA
+from .saya.builtins.broadcast import ListenerSchema  # NOQA
+from .util.async_exec import cpu_bound, io_bound  # NOQA
 
 channel = Channel.current()
+
+onGroupMessage = channel.use(ListenerSchema(listening_events=[GroupMessage]))
+onLoadComplete = channel.use(ListenerSchema(listening_events=[LoadComplete]))
+onGroupNameChange = channel.use(ListenerSchema(listening_events=[GroupNameChange]))
+onMessageSend = channel.use(ListenerSchema(listening_events=[MessageSend]))
+onNoMessageSend = channel.use(ListenerSchema(listening_events=[NoMessageSend]))
+onChatQuit = channel.use(ListenerSchema(listening_events=[ChatQuit]))
+onChatKick = channel.use(ListenerSchema(listening_events=[ChatKick]))
+onGroupDisband = channel.use(ListenerSchema(listening_events=[GroupDisband]))
